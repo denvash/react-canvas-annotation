3ac13dcacdbe4e7d3ff33ee80b83dc317c8d86ea
denvash <justdenva@gmail.com>
2020-05-02 19:55:10 +0300

    Updates

---
+++ precache-manifest.a2e9da9924001a6fb6270cf3b47fdbde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9fdbd245c1ad854aba3eaac15a8c19e",
+    "url": "/react-canvas-annotation/index.html"
+  },
+  {
+    "revision": "603ed30b9f905a5ba5ee",
+    "url": "/react-canvas-annotation/static/css/2.13c6718b.chunk.css"
+  },
+  {
+    "revision": "603ed30b9f905a5ba5ee",
+    "url": "/react-canvas-annotation/static/js/2.a308547b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-canvas-annotation/static/js/2.a308547b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "955c45e21a4d3157f3e5",
+    "url": "/react-canvas-annotation/static/js/main.4487884a.chunk.js"
+  },
+  {
+    "revision": "00184e59ec1e364a781c",
+    "url": "/react-canvas-annotation/static/js/runtime-main.d3be66e0.js"
+  },
+  {
+    "revision": "c13c8705f003201257bd7ce328797bcf",
+    "url": "/react-canvas-annotation/static/media/img.c13c8705.jpg"
+  }
+]);
