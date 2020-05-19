69b4648100a2992056a0597ab21160616708f4c9
denvash <justdenva@gmail.com>
2020-05-19 21:10:38 +0300

    Updates

--- precache-manifest.6e6265dcd7c492bec1712b0600b3572c.js
+++ precache-manifest.6e6265dcd7c492bec1712b0600b3572c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eeb9a0a39d23e2b879f9837489af3dea",
+    "revision": "e485baba3bf45774ad47fca7ba8016da",
     "url": "/react-canvas-annotation/index.html"
   },
   {
-    "revision": "e3e181c3f536098e23f1",
+    "revision": "7452d87aa4c9d7bbca99",
     "url": "/react-canvas-annotation/static/css/2.13c6718b.chunk.css"
   },
   {
-    "revision": "e3e181c3f536098e23f1",
-    "url": "/react-canvas-annotation/static/js/2.2d426bf5.chunk.js"
+    "revision": "7452d87aa4c9d7bbca99",
+    "url": "/react-canvas-annotation/static/js/2.42bff395.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/react-canvas-annotation/static/js/2.2d426bf5.chunk.js.LICENSE.txt"
+    "url": "/react-canvas-annotation/static/js/2.42bff395.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "61389e457661e6172ba8",
-    "url": "/react-canvas-annotation/static/js/main.65babb89.chunk.js"
+    "revision": "9f2ff7ae6161e5e65d9c",
+    "url": "/react-canvas-annotation/static/js/main.fbb12e39.chunk.js"
   },
   {
     "revision": "00184e59ec1e364a781c",
